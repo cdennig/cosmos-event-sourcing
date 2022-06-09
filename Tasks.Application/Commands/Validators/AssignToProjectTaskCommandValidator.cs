cb437c345262fe139bf1c0e11cff197e49080fde
Christian Dennig <christian.dennig@outlook.com>
2022-06-09 20:08:28 +0200

    Add task command validators

---
+++ Tasks.Application/Commands/Validators/AssignToProjectTaskCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Tasks.Application.Commands.Validators;
+
+public class AssignToProjectTaskCommandValidator : AbstractValidator<AssignToProjectTaskCommand>
+{
+    public AssignToProjectTaskCommandValidator()
+    {
+        RuleFor(x => x.TenantId).NotNull().NotEqual(Guid.Empty);
+        RuleFor(x => x.PrincipalId).NotNull().NotEqual(Guid.Empty);
+        RuleFor(x => x.Id).NotNull().NotEqual(Guid.Empty);
+        RuleFor(x => x.ProjectId).NotNull().NotEqual(Guid.Empty);
+    }
+}
---
+++ Tasks.Application/Commands/Validators/CreateTaskCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Tasks.Application.Commands.Validators;
+
+public class CreateTaskCommandValidator : AbstractValidator<CreateTaskCommand>
+{
+    public CreateTaskCommandValidator()
+    {
+        RuleFor(x => x.TenantId).NotNull().NotEqual(Guid.Empty);
+        RuleFor(x => x.PrincipalId).NotNull().NotEqual(Guid.Empty);
+        RuleFor(x => x.Title).NotEmpty().MaximumLength(255);
+        RuleFor(x => x.Description).MaximumLength(4000);
+        // EndDate may be empty, if not, StartDate must have a valid value
+        When(x => x.EndDate.HasValue, () => { RuleFor(x => x.StartDate).NotEmpty().LessThan(x => x.EndDate); });
+        When(x => x.ProjectId.HasValue, () => { RuleFor(x => x.ProjectId).NotNull().NotEqual(Guid.Empty); });
+    }
+}
